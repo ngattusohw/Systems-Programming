83c255d4d037b4468dca1eed7755299689963182
ngattusohw <ngattusohw@gmail.com>
2017-09-14 15:32:27 -0400

    adding my_revstr and my_strrindex

---
+++ libmy1/src/my/my_revstr.c
@@ -0,0 +1,18 @@
+#include "../../include/my.h"
+
+int my_revstr(char* str){
+	int x = 0;
+	if(str && my_strlen(str)>0){
+		int y = my_strlen(str) -1;
+		while(x!=y){
+			char hold = str[x];
+			str[x] = str[y];
+			str[y] = hold;
+			y--;
+			x++;
+		}
+	return my_strlen(str);
+	}else{
+		return -1;
+	}
+}
--- libmy1/test/basic_tests.c
+++ libmy1/test/basic_tests.c
@@ -1,4 +1,6 @@
 #include "../include/my.h"
+#include <string.h>
+#include <stdio.h>
 
 int main(){
 	//my_alpha();
@@ -13,6 +15,17 @@ int main(){
 	char it = 'a';
 	my_int(my_strindex(c,it));
 
+	write(1,"\n",1);
+
+	my_int(my_strrindex("aabbac",'d'));
+
+	write(1,"\n",1);
+
+	char hi[6];
+	strcpy(hi,"");
+	my_int(my_revstr(hi));
+
+	write(1,"\n",1);
 
 	return(0);
 }
