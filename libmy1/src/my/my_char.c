44696ba2862ad6790f4435f517a75b57aac3dffe
ngattusohw <ngattusohw@gmail.com>
2017-09-14 16:22:28 -0400

    Final changes again

--- libmy1/src/my/my_char.c
+++ libmy1/src/my/my_char.c
@@ -1,14 +1,5 @@
 #include "../../include/my.h"
 
 void my_char(char c){
-	/*Write the contents of c to standard out, which is file descriptor 1*/
 	write(1, &c, 1);
 }
-
-
-// int main() {
-// 	char a = '1';
-// 	my_char(a);
-
-// 	return(0);
-// }
--- libmy1/src/my/my_int.c
+++ libmy1/src/my/my_int.c
@@ -3,11 +3,3 @@
 void my_int(int i){
 	my_num_base(i, "0123456789");
 }
-
-
-// int main(){
-// 	int a = 66;
-// 	my_int(a);
-
-// 	return(0);
-// }
