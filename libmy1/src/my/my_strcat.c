444405724f047ee70c8e7286d4f935e8591d51b3
ngattusohw <ngattusohw@gmail.com>
2017-09-21 00:56:47 -0400

    Adding two more untested functions

--- libmy1/src/my/my_strcat.c
+++ libmy1/src/my/my_strcat.c
@@ -1,5 +1,25 @@
 #include "../../include/my.h"
 
-char *my_strcat(char *dst, char *src){
-	
+/*
+Copies src onto the end of dst and returns dst
+Does not allocate memory
+Assumes there is enough memory allocated in dst to hold both strings
+Overwrites src's original '\0' and places a new '\0' onto the end
+ */
+
+char* my_strcat(char *dst, char *src){
+	if(dst && src){
+		int dst_len = my_strlen(dst);
+		int src_len = my_strlen(src);
+		int y=0;
+		for(int x=dst_len;x<(dst_len + src_len);x++){
+			dst[x] = src[y];
+			y++;
+		}
+		dst[dst_len + src_len] = '\\';
+		dst[dst_len + src_len + 1]; = '0';
+		return dst;
+	}else{
+		return NULL;
+	}
 }
