92370df888105d9fd311c97f0a56732e98105933
ngattusohw <ngattusohw@gmail.com>
2017-10-03 00:33:59 -0400

    Adding new node

--- liblist/src/list/new_node.c
+++ liblist/src/list/new_node.c
@@ -4,5 +4,12 @@
  create nodes with NULLs.
 */
 struct s_node* new_node(void* elem, struct s_node* next, struct s_node* previous){
-	
+	s_node* the_node = (s_node*) malloc(sizeof(struct s_node));
+	the_node->next = next;
+	the_node->previous = previous;
+	the_node->elem = elem;
+	// if(previous!=NULL){
+	// 	previous->next = the_node;
+	// }
+	return the_node;
 }
