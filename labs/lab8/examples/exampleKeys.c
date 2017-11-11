85e0ae860520ca6da2e2de0119bfed6b62cc625d
ngattusohw <ngattusohw@gmail.com>
2017-11-10 22:20:38 -0500

    Changing lab8

--- labs/lab8/examples/exampleKeys.c
+++ labs/lab8/examples/exampleKeys.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+
 int main(void) {
 	struct termios initial_settings, new_settings;
 	char key[6] = "x";
@@ -27,14 +28,19 @@ int main(void) {
 		if (key[0] == '\015')
 			strcpy(key, "Enter");
 		else if (strcmp(key, "^[[A") == 0)
-			strcpy(key, "Up");
+			strcpy(key, "DICK");
 		else if (strcmp(key, "^[[B") == 0)
 			strcpy(key, "Down");
 		else if (strcmp(key, "^[[D") == 0)
 			strcpy(key, "Left");
 		else if (strcmp(key, "^[[C") == 0)
 			strcpy(key, "Right");
+		else if(strcmp(key, "^r") == 0)
+			strcpy(key, "FUCK");
 		printf("\nYou entered %s\n", key);
+
+		int s = getchar();
+		printf("%d\n", s);
 	}
 	tcsetattr(fileno(stdin), TCSANOW, &initial_settings);
 	exit(0);
