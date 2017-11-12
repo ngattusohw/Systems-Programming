f5d0b0c8f8c0f94363390071f2675e8440958523
ngattusohw <ngattusohw@gmail.com>
2017-11-12 17:31:22 -0500

    Sending over main_tests to docker env to test

--- liblist/src/list/main_tests.c
+++ liblist/src/list/main_tests.c
@@ -1,6 +1,16 @@
 #include "my.h"
 #include "list.h"
+#include <string.h>
 #include <stdio.h>
-#include <stdlib.h>
 
+int main(){
+	
+	struct s_node* Node = new_node("Hi",NULL,NULL);
 
+	//printf(elem_at(Node,0));
+	
+	struct s_node** the_list = (struct s_node**) malloc(sizeof(struct s_node) * 4);
+	add_node(Node,the_list);
+
+	return(0);
+}
