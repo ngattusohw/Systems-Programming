f9da02095af698cd1bbeb1d8481a63470ccf20dd
ngattusohw <ngattusohw@gmail.com>
2017-11-12 22:10:35 +0000

    Cleaing up liblist

--- liblist/src/list/my_int.c
+++
@@ -1,5 +0,0 @@
-#include "../../include/my.h"
-
-void my_int(int i){
-	my_num_base(i, "0123456789");
-}
--- liblist/src/list/my_str.c
+++
@@ -1,9 +0,0 @@
-#include "../../include/my.h"
-
-void my_str(char* c){
-	if(c==NULL){
-		return;
-	}else{
-		write(1,c,my_strlen(c));
-	}
-}
